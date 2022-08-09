21a3ee5b09b88f0cc80bb763d73328a7db9bc75b
yiming-ni <nym_claire@berkeley.edu>
2022-08-09 15:17:03 -0700

    add dribbling correct navi

---
+++ isaacgymenvs/tasks/a1_dribbling.py
@@ -0,0 +1,361 @@
+import torch
+
+from isaacgym import gymapi
+from isaacgym import gymtorch
+
+from .amp.a1_base import A1Base, dof_to_obs
+from .a1_amp import A1AMP
+from .a1_navigation import A1Navigation
+from .amp.utils_amp import gym_util
+from .amp.utils_amp.motion_lib import A1MotionLib
+
+from isaacgym.torch_utils import *
+from isaacgymenvs.utils.torch_jit_utils import *
+
+ADDITIONAL_OBS_NUM = 3 + 4 + 3 + 3 + 2  # local pos, quat, linear vel, angular vel of the ball, goal pos
+BALL_RAD = 1
+
+
+class A1Dribbling(A1AMP):
+    def __init__(self, cfg, sim_device, graphics_device_id, headless):
+        super().__init__(cfg, sim_device, graphics_device_id, headless)
+        # track goal progress
+        self.goal_terminate = torch.randint(100, 200, (self.num_envs,), device=self.device, dtype=torch.int32)
+        self.goal_step = torch.zeros(self.num_envs, dtype=torch.int32, device=self.device)
+
+    def get_obs_size(self):
+        obs_size = super().get_obs_size()
+        return obs_size + ADDITIONAL_OBS_NUM
+
+    def _init_obs_tensors(self):
+        # TODO add ball history obs
+        pass
+
+    def _get_rigid_body_states_from_tensor(self, states_tensor):
+        pass
+
+    def _get_root_states_from_tensor(self, state_tensor):
+        self.all_actor_indices = torch.arange(self.num_envs * (self.num_markers + 1), dtype=torch.int32,
+                                              device=self.device).reshape(
+            (self.num_envs, (self.num_markers + 1))
+        )
+        self._all_actor_root_states = gymtorch.wrap_tensor(state_tensor)
+        self._root_states = self._all_actor_root_states.view(self.num_envs, self.num_markers + 1, self.num_dof + 1)[
+                            ..., 0, :]
+        self._ball_root_states = self._all_actor_root_states.view(self.num_envs, self.num_markers + 1,
+                                                                  self.num_dof + 1)[..., 1, :]
+        self._ball_root_states[:, :3] = self.initial_ball_pos
+        self._ball_root_states[:, 3:] = 0
+        self._prev_ball_states = self._ball_root_states.clone()
+        if not self.headless:
+            self._goal_root_states = self._all_actor_root_states.view(self.num_envs, self.num_markers + 1, self.num_dof + 1)[..., 2, :]
+        return
+
+    def _create_marker_actors(self, env_ptr, marker_asset, init_goal_pos, i):
+        ball_asset, ball_init_pos = marker_asset[0], init_goal_pos[0]
+        ball_init_pos.p.x = self.initial_ball_pos[i, 0]
+        ball_init_pos.p.y = self.initial_ball_pos[i, 1]
+        ball_init_pos.p.z = self.initial_ball_pos[i, 2]
+        ball_handle = self.gym.create_actor(env_ptr, ball_asset, ball_init_pos, "ball", i, 0, 0)
+        self.gym.set_rigid_body_color(env_ptr, ball_handle, 0, gymapi.MESH_VISUAL_AND_COLLISION,
+                                      gymapi.Vec3(1, 1, 0))
+        if not self.headless:
+            goal_asset, goal_init_pos = marker_asset[1], init_goal_pos[1]
+            goal_init_pos.p.x = self._goal_pos[i, 0]
+            goal_init_pos.p.y = self._goal_pos[i, 1]
+            goal_init_pos.p.z = 0.2
+            goal_handle = self.gym.create_actor(env_ptr, goal_asset, goal_init_pos, "goal", i+self.num_envs, 1, 0)
+            self.gym.set_rigid_body_color(env_ptr, goal_handle, 0, gymapi.MESH_VISUAL_AND_COLLISION,
+                                          gymapi.Vec3(1, 0, 0))
+        return
+
+    def _create_marker_envs(self):
+        asset, pos = [], []
+        # set initial ball pos
+        _ball_dist = torch.rand((self.num_envs, 1), dtype=torch.float, device=self.device) * 3.0
+        _ball_angle = torch.rand((self.num_envs, 1), dtype=torch.float, device=self.device) * torch.pi * 2
+        self.initial_ball_pos = torch.zeros((self.num_envs, 3), dtype=torch.float, device=self.device)
+        self.initial_ball_pos[..., 0] = torch.flatten(_ball_dist * torch.cos(_ball_angle))
+        self.initial_ball_pos[..., 1] = torch.flatten(_ball_dist * torch.sin(_ball_angle))
+        self.initial_ball_pos[..., 2] = BALL_RAD
+
+        ball_asset_opts = gymapi.AssetOptions()
+        ball_asset_opts.fix_base_link = True
+        ball_asset = self.gym.create_sphere(self.sim, BALL_RAD, ball_asset_opts)
+        init_ball_pos = gymapi.Transform()
+        self.num_markers = 1
+        asset.append(ball_asset)
+        pos.append(init_ball_pos)
+        # create goal pos near the ball
+        _goal_dist = torch.rand((self.num_envs, 1), dtype=torch.float, device=self.device) * 5.0
+        _goal_rot = torch.rand((self.num_envs, 1), dtype=torch.float, device=self.device) * torch.pi * 2
+        self._goal_pos = torch.zeros((self.num_envs, 2), dtype=torch.float, device=self.device)
+        self._goal_pos[..., 0] = torch.flatten(_goal_dist * torch.cos(_goal_rot)) + self.initial_ball_pos[..., 0]
+        self._goal_pos[..., 1] = torch.flatten(_goal_dist * torch.sin(_goal_rot)) + self.initial_ball_pos[..., 1]
+        if not self.headless:
+            goal_asset_opts = gymapi.AssetOptions()
+            goal_asset_opts.fix_base_link = True
+            goal_asset = self.gym.create_sphere(self.sim, 0.03, goal_asset_opts)
+            init_goal_pos = gymapi.Transform()
+            self.num_markers += 1
+            asset.append(goal_asset)
+            pos.append(init_goal_pos)
+        return asset, pos
+
+    def _set_env_state(self, env_ids, root_pos, root_rot, dof_pos, root_vel, root_ang_vel, dof_vel):
+        self._root_states[env_ids, 0:3] = root_pos
+        self._root_states[env_ids, 3:7] = root_rot
+        self._root_states[env_ids, 7:10] = root_vel
+        self._root_states[env_ids, 10:13] = root_ang_vel
+        self._dof_pos[env_ids] = dof_pos
+        self._dof_vel[env_ids] = dof_vel
+
+        actor_indices = self.all_actor_indices[env_ids, 0].flatten()
+        ball_indices = self.all_actor_indices[env_ids, 1].flatten()
+        self.gym.set_actor_root_state_tensor_indexed(self.sim, gymtorch.unwrap_tensor(self._all_actor_root_states),
+                                                     gymtorch.unwrap_tensor(actor_indices), len(actor_indices))
+        self.gym.set_actor_root_state_tensor_indexed(self.sim, gymtorch.unwrap_tensor(self._all_actor_root_states),
+                                                     gymtorch.unwrap_tensor(ball_indices), len(ball_indices))
+        self.gym.set_dof_state_tensor_indexed(self.sim, gymtorch.unwrap_tensor(self._dof_state),
+                                              gymtorch.unwrap_tensor(actor_indices), len(actor_indices))
+
+    def _compute_reward(self, actions):
+        self.rew_buf[:] = compute_a1_reward(self._root_states[:, :2],
+                                            self._prev_root_states[:, :2],
+                                            self._goal_pos,
+                                            self._ball_root_states[:, :2],
+                                            self._prev_ball_states[:, :2],
+                                            self.dt,
+                                            self.device)
+        return
+
+    def _compute_a1_obs_full_states(self, env_ids=None):
+        if (env_ids is None):
+            goal_pos = torch.zeros((self.num_envs, 3), dtype=torch.float, device=self.device)
+            goal_pos[..., 2] = 0.2
+            root_states = self._root_states
+            dof_pos = self._dof_pos
+            dof_vel = self._dof_vel
+            key_body_pos = self._rigid_body_pos[:, self._key_body_ids, :]
+            goal_pos[..., :2] = self._goal_pos
+            ball_states = self._ball_root_states
+        else:
+            goal_pos = torch.zeros((len(env_ids), 3), dtype=torch.float, device=self.device)
+            goal_pos[..., 2] = 0.2
+            root_states = self._root_states[env_ids]
+            dof_pos = self._dof_pos[env_ids]
+            dof_vel = self._dof_vel[env_ids]
+            key_body_pos = self._rigid_body_pos[env_ids][:, self._key_body_ids, :]
+            goal_pos[..., :2] = self._goal_pos[env_ids]
+            ball_states = self._ball_root_states[env_ids]
+
+        obs = compute_a1_observations(root_states, dof_pos, dof_vel,
+                                      key_body_pos, self._local_root_obs, goal_pos, ball_states)
+        return obs
+
+    def reset_idx(self, env_ids):
+        self._reset_ball_pos(env_ids)
+        self._reset_goal_pos(env_ids)
+        super().reset_idx(env_ids)
+        return
+
+    def post_physics_step(self):
+        self.goal_step += 1
+        self._prev_root_states[:] = self._root_states[:]
+        self._prev_ball_states[:] = self._ball_root_states[:]
+        super().post_physics_step()
+        # reset goal pos
+        goal_reset_envs = (self.goal_step >= self.goal_terminate).nonzero(as_tuple=False).flatten()
+        if len(goal_reset_envs) > 0:
+            # print('reset encountered!')
+            self._reset_goal_pos(goal_reset_envs)
+        return
+
+    def _reset_ball_pos(self, env_ids):
+        ball_dist = torch.rand((len(env_ids), 1), dtype=torch.float, device=self.device) * 5.0
+        ball_rot = torch.rand((len(env_ids), 1), dtype=torch.float, device=self.device) * torch.pi * 2
+        self.initial_ball_pos[env_ids, 0] = torch.flatten(ball_dist * torch.cos(ball_rot)) + self._root_states[
+            env_ids, 0]*0
+        self.initial_ball_pos[env_ids, 1] = torch.flatten(ball_dist * torch.sin(ball_rot)) + self._root_states[
+            env_ids, 1]*0
+        self.initial_ball_pos[env_ids, 2] = BALL_RAD
+        self._ball_root_states[env_ids, :3] = self.initial_ball_pos[env_ids, :]
+        self._ball_root_states[env_ids, 3:] = 0.
+        return
+
+    def _reset_goal_pos(self, goal_reset_envs):
+        self.goal_terminate[goal_reset_envs] = torch.randint(100, 200, (len(goal_reset_envs),), device=self.device,
+                                                             dtype=torch.int32)
+        self.goal_step[goal_reset_envs] = 0
+        goal_dist = torch.rand((len(goal_reset_envs), 1), dtype=torch.float, device=self.device) * 5.0
+        goal_rot = torch.rand((len(goal_reset_envs), 1), dtype=torch.float, device=self.device) * torch.pi * 2
+        self._goal_pos[goal_reset_envs, 0] = torch.flatten(goal_dist * torch.cos(goal_rot)) + self.initial_ball_pos[goal_reset_envs, 0]
+        self._goal_pos[goal_reset_envs, 1] = torch.flatten(goal_dist * torch.sin(goal_rot)) + self.initial_ball_pos[goal_reset_envs, 1]
+        if not self.headless:
+            actor_indices = self.all_actor_indices[goal_reset_envs, 1].flatten()
+            self._goal_root_states[goal_reset_envs, :2] = self._goal_pos[goal_reset_envs]
+            self.gym.set_actor_root_state_tensor_indexed(self.sim, gymtorch.unwrap_tensor(self._all_actor_root_states),
+                                                         gymtorch.unwrap_tensor(actor_indices), len(actor_indices))
+        return
+
+
+#####################################################################
+###=========================jit functions=========================###
+#####################################################################
+
+@torch.jit.script
+def dof_to_obs(pose):
+    # type: (Tensor) -> Tensor
+    dof_obs_size = 12
+    dof_offsets = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    num_joints = len(dof_offsets) - 1
+
+    dof_obs_shape = pose.shape[:-1] + (dof_obs_size,)
+    dof_obs = torch.zeros(dof_obs_shape, device=pose.device)
+    dof_obs_offset = 0
+
+    for j in range(num_joints):
+        dof_offset = dof_offsets[j]
+        dof_size = dof_offsets[j + 1] - dof_offsets[j]
+        joint_pose = pose[:, dof_offset:(dof_offset + dof_size)]
+
+        # assume this is a spherical joint
+        if (dof_size == 3):
+            joint_pose_q = exp_map_to_quat(joint_pose)
+            joint_dof_obs = quat_to_tan_norm(joint_pose_q)
+            dof_obs_size = 6
+        else:
+            joint_dof_obs = joint_pose
+            dof_obs_size = 1
+
+        dof_obs[:, dof_obs_offset:(dof_obs_offset + dof_obs_size)] = joint_dof_obs
+        dof_obs_offset += dof_obs_size
+
+    return dof_obs
+
+
+@torch.jit.script
+def compute_a1_observations(root_states, dof_pos, dof_vel, key_body_pos, local_root_obs, goal_pos, ball_states):
+    # type: (Tensor, Tensor, Tensor, Tensor, bool, Tensor, Tensor) -> Tensor
+    root_pos = root_states[:, 0:3]
+    root_rot = root_states[:, 3:7]
+    root_vel = root_states[:, 7:10]
+    root_ang_vel = root_states[:, 10:13]
+    ball_pos = ball_states[:, 0:3]
+    ball_rot = ball_states[:, 3:7]
+    ball_vel = ball_states[:, 7:10]
+    ball_ang_vel = ball_states[:, 10:13]
+
+    root_h = root_pos[:, 2:3]
+    heading_rot = calc_heading_quat_inv(root_rot)
+
+    if (local_root_obs):
+        root_rot_obs = quat_mul(heading_rot, root_rot)
+    else:
+        root_rot_obs = root_rot
+    root_rot_obs = quat_to_tan_norm(root_rot_obs)
+
+    local_root_vel = my_quat_rotate(heading_rot, root_vel)
+    local_root_ang_vel = my_quat_rotate(heading_rot, root_ang_vel)
+
+    root_pos_expand = root_pos.unsqueeze(-2)
+    local_key_body_pos = key_body_pos - root_pos_expand
+
+    heading_rot_expand = heading_rot.unsqueeze(-2)
+    heading_rot_expand_for_key_body = heading_rot_expand.repeat((1, local_key_body_pos.shape[1], 1))
+    flat_end_pos = local_key_body_pos.view(local_key_body_pos.shape[0] * local_key_body_pos.shape[1],
+                                           local_key_body_pos.shape[2])
+    flat_heading_rot = heading_rot_expand_for_key_body.view(heading_rot_expand_for_key_body.shape[0] * heading_rot_expand_for_key_body.shape[1],
+                                               heading_rot_expand_for_key_body.shape[2])
+    local_end_pos = my_quat_rotate(flat_heading_rot, flat_end_pos)
+    flat_local_key_pos = local_end_pos.view(local_key_body_pos.shape[0],
+                                            local_key_body_pos.shape[1] * local_key_body_pos.shape[2])
+
+    local_goal_pos = goal_pos - root_pos
+    local_target_pos = my_quat_rotate(heading_rot, local_goal_pos)
+    flat_local_goal_xy = local_target_pos[:, :2]
+
+    # ground truth obs for the ball
+    local_ball_pos = ball_pos - root_pos
+    local_ball_pos = my_quat_rotate(heading_rot, local_ball_pos)
+
+    local_ball_rot = quat_mul(heading_rot, ball_rot)
+
+    local_ball_vel = my_quat_rotate(heading_rot, ball_vel)
+
+    local_ball_ang_vel = my_quat_rotate(heading_rot, ball_ang_vel)
+
+    dof_obs = dof_to_obs(dof_pos)
+
+    obs = torch.cat((root_h, root_rot_obs, local_root_vel, local_root_ang_vel, dof_obs, dof_vel, flat_local_key_pos,
+                     flat_local_goal_xy, local_ball_pos, local_ball_rot, local_ball_vel, local_ball_ang_vel),
+                    dim=-1)
+    return obs
+
+
+@torch.jit.script
+def compute_a1_reward(root_xy, prev_root_xy, goal_xy, ball_xy, prev_ball_xy, dt, device):
+    # type: (Tensor, Tensor, Tensor, Tensor, Tensor, float, Optional[Device]) -> Tensor
+
+    v1 = (root_xy[:, 0] - prev_root_xy[:, 0]) / dt
+    v2 = (root_xy[:, 1] - prev_root_xy[:, 1]) / dt
+    v1_ball = (ball_xy[:, 0] - prev_ball_xy[:, 0]) / dt
+    v2_ball = (ball_xy[:, 1] - prev_ball_xy[:, 1]) / dt
+
+    diff_b = ball_xy - root_xy
+    dist_b = diff_b[:, 0] ** 2 + diff_b[:, 1] ** 2
+    dist_b_reward = torch.exp(- dist_b * 0.5)
+
+    d_ball = diff_b / torch.sqrt_(dist_b).reshape(-1, 1)
+    actor_vel_reward = torch.exp(
+        - 1.5 * torch.maximum(torch.zeros_like(v1, dtype=torch.float, device=device),
+                        1.0 - (d_ball[:, 0] * v1 + d_ball[:, 1] * v2)) ** 2)
+
+    x_diff = goal_xy[:, 0] - root_xy[:, 0]
+    y_diff = goal_xy[:, 1] - root_xy[:, 1]
+    dist = x_diff * x_diff + y_diff * y_diff
+    dist_reward = torch.exp(- dist * 0.5)
+
+
+    d_len = torch.sqrt_(dist)
+    d1 = x_diff / d_len
+    d2 = y_diff / d_len
+    ball_vel_reward = torch.exp(
+        - torch.maximum(torch.zeros_like(v1, dtype=torch.float, device=device),
+                        1.0 - (d1 * v1_ball + d2 * v2_ball)) ** 2)
+    ball_vel_reward = torch.where(dist > 0.25, ball_vel_reward, torch.ones_like(ball_vel_reward, dtype=torch.float, device=device))
+    reward = 0.1 * actor_vel_reward + 0.1 * dist_b_reward + 0.3 * ball_vel_reward + 0.5 * dist_reward
+    return reward
+
+
+@torch.jit.script
+def compute_a1_reset(reset_buf, progress_buf, contact_buf, contact_body_ids, rigid_body_pos,
+                     max_episode_length, enable_early_termination, termination_height):
+    # type: (Tensor, Tensor, Tensor, Tensor, Tensor, float, bool, float) -> Tuple[Tensor, Tensor]
+    terminated = torch.zeros_like(reset_buf)
+
+    if (enable_early_termination):
+        masked_contact_buf = contact_buf.clone()
+        masked_contact_buf[:, contact_body_ids, :] = 0
+        fall_contact = torch.any(masked_contact_buf > 0.1, dim=-1)
+        fall_contact = torch.any(fall_contact, dim=-1)
+
+        body_height = rigid_body_pos[..., 2]
+        fall_height = body_height < termination_height
+        fall_height[:, contact_body_ids] = False
+        fall_height = torch.any(fall_height, dim=-1)
+
+        has_fallen = torch.logical_and(fall_contact, fall_height)
+
+        # first timestep can sometimes still have nonzero contact forces
+        # so only check after first couple of steps
+        has_fallen *= (progress_buf > 1)
+        terminated = torch.where(has_fallen, torch.ones_like(reset_buf), terminated)
+
+    reset = torch.where(progress_buf >= max_episode_length - 1, torch.ones_like(reset_buf), terminated)
+
+    return reset, terminated
+
+
+
+
